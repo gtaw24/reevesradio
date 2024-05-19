351581ee2eb163bac2df747e272742b02bac6315
gtaw24 <131622601+gtaw24@users.noreply.github.com>
2024-05-19 20:19:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('.btn');
     const player = document.getElementById('player');
+    const playerContainer = document.getElementById('player-container');
 
     buttons.forEach(button => {
         button.addEventListener('click', function() {
             const stream = this.getAttribute('data-stream');
             player.src = stream;
-            player.hidden = false;
+            playerContainer.hidden = false;
             player.play();
         });
     });
